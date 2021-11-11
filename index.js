6b3e381a50498c76fc489e80b4e66f17dd6cd956
Bryson Best <bryson.best@gmail.com>
2021-11-10 16:55:59 -0800

    added modular code to support transactions.

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
+   // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransactions(this);
+  }
+}
+
+class Withdrawal extends Transaction{
+  // Update the balance in the account
+  get value() {
+    return -(this.amount);
+  }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
   }
 
-  commit() {
-    balance -= this.amount;
+}
+
+class Deposit extends Transaction{
+  // Update the balance in the account
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let bal = 0;
+    for (let item of this.transactions) {
+      bal += item.value;
+    }
+    return bal;
+  }
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
@@ -17,13 +60,18 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(70, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Transaction 2:', t);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Ending Balance:', myAccount.balance);
